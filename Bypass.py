b02c3ffd3f552b39835beb028dddd23743fc6256
Mrbaloch356 <102709281+Mrbaloch356@users.noreply.github.com>
2022-10-28 23:33:53 +0500

    Create Bypass.py

---
+++ Bypass.py
@@ -0,0 +1,30 @@
+import os,time,platform
+
+os.system('clear')
+
+print('[•] Checking Updates...')
+
+os.system('git pull')
+
+green = ('\033[1;32m')
+
+white = ('\033[1;37m')
+
+red = ('\033[1;31m')
+
+ 
+
+print('<------------------------------------>')
+
+bit = platform.architecture()[0]
+
+if bit=='64bit':
+
+    print(f'{red}[•] BYPASS BY UNKNOWN {white}')
+    print(f'{red}[•] Join Over GROUP {white}')
+
+    os.system('xdg-open https://chat.whatsapp.com/GXiVEsLjhjq1P4pOno8YHB')
+
+    import v14
+
+v14.sdj() 
